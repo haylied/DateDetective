06fa13cf166db41779d0c731578d1a16b5eaf19a
haylied <hayliedarland@gmail.com>
2024-07-15 20:19:13 -0500

    AssemblyInfo Cache Updates, I think

--- DDetective/DDetective/obj/Debug/net6.0/DDetective.AssemblyInfo.cs
+++ DDetective/DDetective/obj/Debug/net6.0/DDetective.AssemblyInfo.cs
@@ -11,12 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-DDetective-c20e12ae-67fa-4c47-ae4c-4711a6b55763")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDetective")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91702da4fd4e6266c0c64c6166b9565f874824e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDetective")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDetective")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
