ce0b67167308279b41681c1fe11acfae1fb8b6f6
haylied <hayliedarland@gmail.com>
2025-02-10 15:48:09 -0600

    Fixed HttpClient Issues, Modification to app.config.ts

--- DDetective/Client/DateDetectiveClient/src/app/app.config.ts
+++ DDetective/Client/DateDetectiveClient/src/app/app.config.ts
@@ -4,13 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withInterceptorsFromDi())]
 };
 
 export const AppConfig = {
-  apiUrl: 'https://api.example.com', //server URL
+  apiUrl: 'http://localhost:16725', //server URL
   appName: 'Event Manager',
   version: '1.0.0',
 };
