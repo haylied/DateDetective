ea68efc0b096b40ddae70179f4c3e8d521663a7d
haylied <hayliedarland@gmail.com>
2025-02-19 14:35:08 -0600

    Updating AddEventModel & AddEventView Model Fields in Order to Test Migration & Tables

--- DDetective/Server/DDetective/ViewModels/AddEventViewModel.cs
+++ DDetective/Server/DDetective/ViewModels/AddEventViewModel.cs
@@ -8,17 +8,38 @@ namespace DDetective.ViewModels
     {
         [Key]
         public int EventId { get; set; }
+
+        [Required]
+        [MinLength(3)]
         public string EventName { get; set; }
+
+        [Required]
         public string EventDescription { get; set; }
         // public string EventCategory { get; set; }
         // public string EventType { get; set; }
-        public DateTime DateStart { get; set; }
-        public DateTime DateEnd { get; set; }
-        public DateTime TimeStart { get; set; }
-        public DateTime TimeEnd { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
+        [Required]
+        public DateTime StartDateTime { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
+        [Required]
+        public DateTime EndDateTime { get; set; }
+
+        // !-- Adding New Fields --!
+
+        //public int OriginatorId {  get; set; } // same as userId
+
+        //public string OriginatorName { get; set; } // get value from userId key
+
+        //public List<int> Attendees { get; set; } // list of userIds attending meeting
+
 
         public AddEventViewModel()
         {
+
         }
     }
 }
