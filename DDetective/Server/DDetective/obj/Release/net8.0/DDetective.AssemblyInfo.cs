7d53d3a90bad472eea4ee08cde0346e197b38d65
haylied <hayliedarland@gmail.com>
2025-02-20 14:51:28 -0600

    Update Files

--- DDetective/Server/DDetective/obj/Release/net8.0/DDetective.AssemblyInfo.cs
+++ DDetective/Server/DDetective/obj/Release/net8.0/DDetective.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDetective")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea68efc0b096b40ddae70179f4c3e8d521663a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac8b9ac6d605bd538a1583b8c0c0b3fe0ec9dff")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDetective")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDetective")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
