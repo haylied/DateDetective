673507ffb22ceda86e5f93c20f7d8b64be69105c
haylied <hayliedarland@gmail.com>
2025-02-21 15:47:30 -0600

    Start Placing New Parameters for Application User

---
+++ DDetective/Server/DDetective/Models/ApplicationUser.cs
@@ -0,0 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DDetective.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailConfirmed { get; set; }
+        public DateTime AccountCreationDate { get; set; } = DateTime.Now;
+
+
+        public ApplicationUser()
+        {
+
+        }
+
+        public ApplicationUser(int userId, string firstName, string lastName, string emailConfirmed, DateTime accountCreationDate)
+        {
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            EmailConfirmed = emailConfirmed;
+            AccountCreationDate = accountCreationDate;
+
+
+        }
+    }
+}
--- DDetective/Server/DDetective/obj/Debug/net8.0/DDetective.AssemblyInfo.cs
+++ DDetective/Server/DDetective/obj/Debug/net8.0/DDetective.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDetective")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d53d3a90bad472eea4ee08cde0346e197b38d65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cdaa2e5a150c7f1f7cf008e3bc35ede7e2329ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDetective")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDetective")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
