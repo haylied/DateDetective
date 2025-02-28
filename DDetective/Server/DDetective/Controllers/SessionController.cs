70e4e3a906ac6bb631e11cc5307a861ca8434bfc
haylied <hayliedarland@gmail.com>
2025-02-28 15:47:32 -0600

    Create GenerateSessionToken()

--- DDetective/Server/DDetective/Controllers/SessionController.cs
+++ DDetective/Server/DDetective/Controllers/SessionController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using DDetective.ViewModels;
 using DDetective.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 
 namespace DDetective.Controllers
 {
@@ -44,6 +45,8 @@ namespace DDetective.Controllers
                     ExpirationDate = sessionViewModel.ExpirationDate
                 };
 
+                sessionModel.SessionToken = GenerateSessionToken();
+
                 await sessionContext.Session.AddAsync(sessionModel);
                 await sessionContext.SaveChangesAsync();
 
@@ -84,6 +87,27 @@ namespace DDetective.Controllers
             await sessionContext.SaveChangesAsync();
 
             return NoContent();
+        }
+
+
+        public static string GenerateSessionToken(string charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+        {
+            int length = 10;
+
+            // separates charSet into individual, unique characters... if duplicates exist, use .Distinct()
+            var charArray = charSet.ToArray();
+
+            // sets char array to length of 10
+            char[] token = new char[length];
+
+            // loop 10 times to grab random char from charArray, cryptographically generates random integer from 0 to charArray.Length, sets to token
+            for (int i = 0; i < length; i++)
+            {
+                token[i] = charArray[RandomNumberGenerator.GetInt32(charArray.Length)];
+            }
+
+            // new string created from this array, string is returned
+            return new string(token);
         }
 
     }
