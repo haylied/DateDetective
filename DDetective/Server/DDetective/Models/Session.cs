f6e0043805f121c04874be5e3565bb31a17c497e
haylied <hayliedarland@gmail.com>
2025-03-25 13:10:36 -0500

    Session- ExpriationTime

--- DDetective/Server/DDetective/Models/Session.cs
+++ DDetective/Server/DDetective/Models/Session.cs
@@ -9,17 +9,16 @@ namespace DDetective.Models
         public int SessionId { get; set; }
         public string SessionName { get; set; }
         public string SessionToken { get; set; }
-        public DateTime ExpirationDate { get; set; }
+        public DateTime ExpirationTime { get; set; }
 
         public Session() { }
 
-        public Session(int sessionId, string sessionName, string sessionToken, DateTime expirationDate)
+        public Session(int sessionId, string sessionName, string sessionToken, DateTime expirationTime)
         {
             SessionId = sessionId;
             SessionName = sessionName;
             SessionToken = sessionToken;
-            ExpirationDate = expirationDate;
-        }
+            ExpirationTime = expirationTime;
 
 
     }
